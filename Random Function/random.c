c1605b2d0cb5f5165645d171506dd1ac4c3147a7
Richard Frimpong Karikari <pearlgabby0@gmail.com>
2023-04-13 16:21:51 +0000

    quick fix

--- Random Function/random.c
+++ Random Function/random.c
@@ -5,18 +5,22 @@
 int get_random_index(int array_size) 
 {
 
+    /*This sets a variable */
+    static int seed = 0; 
 
-    static int seed = 0; // Seed for the PRNG
-    seed = (seed + (int)time(NULL)) % 10000; // Generate a new seed based on the system clock
+    /*The code below generate a new seed based on the system clock*/
+    seed = (seed + (int)time(NULL)) % 10000; 
 
     
 
-    /*This will generate a pseudo-random number between 0 and 32767*/
+    /*This will generate a pseudo-random number between 0 and 32767
+    * 
+    */
     int random_num = (seed * 1103515245 + 12345) % 32768; 
 
     int x = random_num % array_size;
     printf("x: %d\n", x);
-    return random_num % array_size; // Return a random index within the bounds of the array
+    return x; // Return a random index within the bounds of the array
 }
 
 
