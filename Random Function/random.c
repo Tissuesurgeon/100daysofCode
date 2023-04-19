414f3b3f7ac9860b535f1eb44a61e53bd7e0627a
Richard Frimpong Karikari <pearlgabby0@gmail.com>
2023-04-19 14:40:05 +0000

    tried a simpler algorithm to fix an error

--- Random Function/random.c
+++ Random Function/random.c
@@ -8,26 +8,29 @@ int get_random_index(int array_size)
 {
 
     /*This sets a variable */
-    static int seed = 0; 
+    //static int seed = 0; 
 
     /*The code below generate a new seed based on the system clock*/
-    seed = (seed + (int)time(NULL)) % 10000; 
+   // seed = (seed + (int)time(NULL)) % 10000; 
 
     
 
     /*This will generate a pseudo-random number between 0 and 32767
     * 
     */
-    int random_num = (seed * 1103515245 + 12345) % 32768; 
+    //int random_num = (seed * 1103515245 + 12345) % 32768; 
+
+    //int x = random_num % array_size;
+    //printf("x: %d\n", x);
+    int random_num = ((int)time(NULL)) % 32768;
 
     int x = random_num % array_size;
-    printf("x: %d\n", x);
     return x; // Return a random index within the bounds of the array
 }
 
 
 int main() {
-    int array[] = {1, 2, 3, 4, 5};
+    int array[] = {49, 56, 84, 83, 90, 400, 201};
     int array_size = sizeof(array) / sizeof(int);
 
     int random_index = get_random_index(array_size);
